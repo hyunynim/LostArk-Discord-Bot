bf10dcdf262572b9b20c5ff4f9686a0e6b3cd39f
Ahn honghyun <32119804+hyunynim@users.noreply.github.com>
2020-03-18 00:50:36 +0900

    modify commands
    길드원 목록 관리를 csv 파일로 하게 변경
    길드원 업데이트 시 해당 파일 업로드 되며 길드원으로 조회시 csv 첨부하여 전송

--- bot.py
+++ bot.py
@@ -20,17 +20,18 @@ async def on_ready():
 async def on_message(message):
         if message.content.startswith("!"):
                 msg = message.content.split(" ")
-                if msg[0] == "!뻐큐":
+                msg[0] = msg[0][1:]
+                if msg[0] == "뻐큐":
                         await message.channel.send("맛있네요!")
-                elif msg[0] == "!공포의로붕이":
+                elif msg[0] == "공포의로붕이":
                         await message.channel.send("저런.. 또 실패하셨나요?")
-                elif msg[0] == "!등장":
+                elif msg[0] == "등장":
                         await message.channel.send("-비슈타르-")
-                elif msg[0] == "!ㅈㅁ":
+                elif msg[0] == "ㅈㅁ":
                         await message.channel.send("마스터께서는 아라드 대륙을 수호하고 계세요!")
-                elif msg[0] == "!함말뚝":
+                elif msg[0] == "함말뚝":
                         await message.channel.send("그는 완벽한 멸시장인이에요!")
-                elif msg[0] == "!단새론":
+                elif msg[0] == "단새론":
                         if len(msg) < 2:
                                 if random.randrange(1, 10) <= 5:
                                         await message.channel.send("그녀는 지금 예민해졌어요!")
@@ -42,19 +43,19 @@ async def on_message(message):
                                         await message.channel.send("단새론 님의 강화 성공확률은 ~~~~ " + repr(r) + " 입니다.")
                                         if r <= 10:
                                                 await message.channel.send("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
-                elif msg[0] == "!이호진":
+                elif msg[0] == "이호진":
                         await message.channel.send("장인의 기운이 쌓이고 있어요!")
-                elif msg[0] == "!전사김종성":
+                elif msg[0] == "전사김종성":
                         await message.channel.send("그는 크고 거대해요!")
-                elif msg[0] == "!전김":
+                elif msg[0] == "전김":
                         await message.channel.send("그는 크고 거대해요!")
-                elif msg[0] == "!당진스라소니":
+                elif msg[0] == "당진스라소니":
                         await message.channel.send("또 쌀먹하러 오셨네요!")
-                elif msg[0] == "!동물맨":
+                elif msg[0] == "동물맨":
                         await message.channel.send("또 쌀먹하러 오셨네요!")
-                elif msg[0] == "!아르카라마":
+                elif msg[0] == "아르카라마":
                         await message.channel.send("모코코 좀 그만 캐세요!")
-                elif msg[0] == "!짱쭌이":
+                elif msg[0] == "짱쭌이":
                         if len(msg) < 2:
                                 await message.channel.send("사사게를 리프레쉬 해봐야겠어요!")
                         else:
@@ -63,9 +64,9 @@ async def on_message(message):
                                         await message.channel.send("짱쭌이 님의 강화 성공확률은 ~~~~ " + repr(r) + " 입니다.")
                                         if r <= 10:
                                                 await message.channel.send("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
-                elif msg[0] == "!비슈타르":
+                elif msg[0] == "비슈타르":
                         await message.channel.send("등장!")
-                elif msg[0] == "!전정":
+                elif msg[0] == "전정":
                         resText = "```"
                         profileUrl = "https://lostark.game.onstove.com/Profile/Character/"
                         await message.channel.send(msg[1] + "님의 전투정보실 링크에요!\n" + profileUrl + msg[1] + "\n각인은 제대로 표시되지 않을 수 있다는 점! 알아두세용!\n")
@@ -135,7 +136,7 @@ async def on_message(message):
                                 resText += "\n"
                         resText += "```"
                         await message.channel.send(resText)
-                elif msg[0] == "!마리":
+                elif msg[0] == "마리":
                         gold = 0
                         if len(msg) < 2:
                                 await message.channel.send("명령어를 !마리 [현재 크리스탈 구매 가격]으로 입력하면 마리의 상점 가격을 골드로 환산할 수 있어요!")
@@ -156,17 +157,17 @@ async def on_message(message):
                                 str += itemName[i] + "[크리스탈 " + itemPrice[i] + "개] = " + repr(int(int(itemPrice[i]) * (gold / 95.0))) + "골드\n"
                         str += "```"
                         await message.channel.send(str)
-                elif msg[0] == "!주사위":
+                elif msg[0] == "주사위":
                         r = random.randrange(1,100)
                         await message.channel.send(message.author.name + "님의 주사위는 ~~~~ " + repr(r) + " 입니다. (1-100)")
                         if r <= 10:
                                 await message.channel.send("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
-                elif msg[0] == "!로붕이":
+                elif msg[0] == "로붕이":
                         r = random.randrange(1,100)
                         await message.channel.send("로붕이 님의 강화 성공확률은 ~~~~ " + repr(r) + " 입니다.")
                         if r <= 10:
                                 await message.channel.send("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
-                elif msg[0] == "!미스틱":
+                elif msg[0] == "미스틱":
                         await message.channel.send("아크 숨결 당첨자는~~~~~~~~~~~")
                         resStr = ""
                         time.sleep(2 + random.randrange(2, 4))
@@ -175,7 +176,7 @@ async def on_message(message):
                         for i in range(0, 3):
                                 resStr += repr(mystic[i]) + "번 "
                         await message.channel.send(":star2: " + resStr + ":star2:")
-                elif msg[0] == "!캘린더":
+                elif msg[0] == "캘린더":
                         url = "http://m.inven.co.kr/lostark/timer/"
                         req = urllib.request.urlopen(url)
                         res = req.read()
@@ -190,7 +191,7 @@ async def on_message(message):
                                 str += "\n"
                         str += "```"
                         await message.channel.send(str)
-                elif msg[0] == "!골드":
+                elif msg[0] == "골드":
                         gold = 0
                         if len(msg) < 2:
                                 await message.channel.send("명령어를 !골드 [크리스탈 판매 가격]으로 입력하면 어떻게 골드를 구매하는 것이 더 흑우가 될 수 있는 지 알 수 있어요!")
@@ -202,7 +203,7 @@ async def on_message(message):
                                 await message.channel.send(str)
                         else:
                                 await message.channel.send("명령어를 !골드 [크리스탈 판매 가격]으로 입력하면 어떻게 골드를 구매하는 것이 더 흑우가 될 수 있는 지 알 수 있어요!")
-                elif msg[0] == "!강화":
+                elif msg[0] == "강화":
                         if len(msg) < 2:
                                 await message.channel.send("명령어를 !강화 [확률]로 입력하면 강화 시뮬레이션을 해볼 수 있어요!")
                         elif msg[1].isdigit():
@@ -212,7 +213,7 @@ async def on_message(message):
                                 await message.channel.send("성공!" if r <= int(msg[1]) else "장인의 기운이 쌓여가네요..")
                         else:
                                 await message.channel.send("명령어를 !골드 [크리스탈 판매 가격]으로 입력하면 어떻게 골드를 구매하는 것이 더 흑우가 될 수 있는 지 알 수 있어요!")
-                elif msg[0] == "!정보":
+                elif msg[0] == "정보":
                         url = "https://github.com/hyunynim/LostArk-Discord-Bot/blob/master/README.md"
                         req = urllib.request.urlopen(url)
                         res = req.read()
@@ -225,25 +226,29 @@ async def on_message(message):
                         str += "```"
                         await message.channel.send(str)
                         await message.channel.send("이 내용은 https://github.com/hyunynim/LostArk-Discord-Bot/blob/master/README.md 에서도 확인할 수 있습니다.")
-                elif msg[0] == "!p" or msg[0] == "!s" or ('0' <= msg[0][1] and msg[0][1] <= '9'):
+                elif msg[0] == "p" or msg[0] == "s" or ('0' <= msg[0][1] and msg[0][1] <= '9'):
                         print('Music bot skip mode')
-                elif msg[0] == "!길드원":
+                elif msg[0] == "길드원":
                         try:
-                                f = open("memberList.txt", 'r')
+                                f = open("memberList.csv", 'r')
                         except:
                                 await message.channel.send("```\n길드원 목록을 불러올 수 없습니다.\n@길드원업데이트 명령어를 이용하여 목록을 업데이트해주세요\n```")
                                 return
                         resText = "```\n현재까지 추가된 길드원 목록입니다.\n"
                         memberList = f.readlines()
                         for member in memberList:
-                                resText += member
+                                l = member.split(',')
+                                for str in l:
+                                        resText += str
+                                        resText += "\t"
                         f.close()
                         resText += "```"
                         await message.channel.send(resText)
-                elif msg[0] == "!길드원업데이트":
+                        await message.channel.send(file=discord.File("memberList.csv"))
+                elif msg[0] == "길드원업데이트":
                         await message.channel.send("```\n길드원 목록을 업데이트합니다.\n업데이트에는 다소 시간이 소요될 수 있습니다.```")
                         f = open("member.txt", 'r')
-                        f2 = open("memberList.txt", 'w')
+                        f2 = open("memberList.csv", 'w')
                         member = f.readlines()
                         for name in member:
                                 profileUrl = "https://lostark.game.onstove.com/Profile/Character/"
@@ -257,11 +262,14 @@ async def on_message(message):
                                 className = [each_line.get_text().strip() for each_line in className[:20]]
                                 levelInfoItem = soup.find_all('div', class_='level-info__item')
                                 levelInfoItem = [each_line.get_text().strip() for each_line in levelInfoItem[:20]]
-                                f2.writelines(name + "\t" + className[0][3:] + "\t" + levelInfoItem[0][6:] + "\n")
+                                levelInfoItem[0] = levelInfoItem[0][6:].replace(",","")
+                                tmpList = levelInfoItem[0].split(".")
+                                level = tmpList[0]
+                                f2.writelines(name + "," + className[0][3:] + "," + level + "\n")
                         f.close()
                         f2.close()
                         await message.channel.send("```\n길드원 목록 업데이트가 완료되었습니다.\n@길드원 명령어를 통해 확인하실 수 있습니다.\n```")
-                elif msg[0] == "!길드원추가":
+                elif msg[0] == "길드원추가":
                         chk = 0
                         f = open("member.txt", 'r')
                         member = f.readlines()
@@ -280,7 +288,7 @@ async def on_message(message):
                                 f.write(msg[1] + "\n")
                                 await message.channel.send("추가되었습니다.")
                                 f.close()
-                elif msg[0] == "!길드원삭제":
+                elif msg[0] == "길드원삭제":
                         f = open("member.txt", 'r')
                         member = f.readlines()
                         f.close()
@@ -292,6 +300,7 @@ async def on_message(message):
                                 else:
                                         f.write(name + "\n")
                         f.close()
+                        await message.channel.send("삭제되었습니다.")
                 else:
                         await message.channel.send(msg)
                         str = "```\n!정보\n*---명령어---*\n"
@@ -299,5 +308,5 @@ async def on_message(message):
                         str += "!강화 [확률]] !길드원 !길드원업데이트 !길드원추가 [닉네임] !길드원삭제 [닉네임]\n\n"
                         str += "*---정보---*\n"
                         str += "!뻐큐 !공포의로붕이 !ㅈㅁ !함말뚝 !단새론 !이호진 !전사김종성 !전김 !당진스라소니 !동물맨 !아르카라마 !짱쭌이 !로붕이```"
-                        await message.channel.send(str
+                        await message.channel.send(str)
 client.run("TOKEN")
